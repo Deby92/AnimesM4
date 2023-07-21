4b984678c1a6d5871b3bb16476d2a5f91183c645
Debora Cantuarias <d.cantuarias.urrutia@gmail.com>
2023-07-20 22:07:28 -0400

    deñete creada

--- index.js
+++ index.js
@@ -67,5 +67,57 @@ app.get('/create', async (req, res) => {
         res.status(500).send(error.message);
     }
     res.end();
-    })
+    });
+    app.get('/update/:id', async(req, res) => {
+        try {
+            const id = req .params.id;
+            const nombre = req.query.nombre;
+            const genero = req.query.genero;
+            const año = req.query.año;
+            const autor = req.query.autor;
+            let encontrado = false;
+
+            const animesRaiz = JSON.parse(await fs.readFile(__dirname + '/animes.json'));
+            let animes = animesRaiz[id];
+            if (animes) {
+                animes.nombre = nombre;
+                animes.genero = genero;
+                animes.año = año;
+                animes.autor = autor;
+                encontrado = true;
+        }
+        if (encontrado) {
+            await fs.writeFile(__dirname + '/animes.json', JSON.stringify(animesRaiz));
+            res.status(201).json(animesRaiz);
+        } else {
+            res.status(404).json({
+                status: 'OK',
+                message: 'No existe anime para actualizar'
+            });
+        }
+    
+    } catch (error){
+        console.log(error);
+        res.status(500).send(error.message);
+    }
+    res.end();
+});
+
+//http://localhost:3000/delete/3
+app.get('/delete/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const animesRaiz = JSON.parce(await fs.readFile(__dirname + '/animes.json'));
+        delete animesRaiz[id];
+        await fs.writeFile(__dirname + '/animes.json', JSON.stringify(animesRaiz));
+        res.status(201).json(animesRaiz);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            status: 'ERROR',
+            message: error.message
+        });
+    }
+    res.end();
+});
 app.listen(PORT, () => console.log(`Iniciando en el puerto ${PORT}`));
